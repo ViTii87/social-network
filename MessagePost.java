a4e32ca07d4b34159ed7b257e2a1d6acb26bda82
Victor <victormel87@gmail.com>
2016-05-13 12:23:30 +0200

    Proyecto con herencia

---
+++ MessagePost.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+/**
+ * Write a description of class MessagePost here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MessagePost extends Post
+{
+    private String message;
+
+    /**
+     * Constructor for objects of class MessagePost
+     */
+    public MessagePost(String username, String text)
+    {
+       super(username);
+       this.message = text;
+    }
+
+    /**
+     * Metodo que devuelve el mensaje del post
+     */
+    public String getText(){
+        return message;
+    }
+    
+    public void printShortSummary(){
+        System.out.println("Esto es un post de texto creado por " + getUsername());
+    }
+}
---
+++ PhotoPost.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+public class PhotoPost extends Post
+{
+    private String filename;
+    private String caption;
+
+    /**
+     * Constructor for objects of class MessagePost
+     */
+    public PhotoPost(String author, String filename, String caption)
+    {
+       super(author);
+       this.filename = filename;
+       this.caption = caption;
+    }
+
+    /**
+     * Meotdo que devuelve el nombre del archivo
+     */
+    public String getImageFile(){
+        return filename;
+    }
+    
+    /**
+     * Metodo que devuelve el mensaje del post
+     */
+    public String getCaption(){
+        return caption;
+    }
+}
